b3dd3b146e9cf6b27e7cbd6c442650ca2c3a7db2
Sandeep Pansari <86551476+sandeep18p@users.noreply.github.com>
2024-02-20 15:09:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 function getFormvalue() {
-    //Write your code here
+   var form = document.getElementById("form1");
 
+    // Get the values of first name and last name from the form
+    var firstName = form.elements["fname"].value;
+    var lastName = form.elements["lname"].value;
+
+    // Concatenate first name and last name
+    var fullName = firstName + " " + lastName;
+
+    // Display the full name using alert
+    alert(fullName);
+
+    // Prevent the default form submission
+    return false;
 }
